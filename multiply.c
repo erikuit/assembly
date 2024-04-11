5f2e6058f0af097885659c6137f9a92ecd09193d
erikuit <erik@becurious.com>
2024-04-11 20:43:52 +0200

    Multiply.c

---
+++ multiply.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main(int argc, char *argv[]) {
+   if (argc < 2) {
+        printf("Usage: %s <multiplier> <multiplicand>\n", argv[0]);
+        return 1;
+    }
+
+    int multiplier = atoi(argv[1]);
+    int multiplicand = atoi(argv[2]);
+    int product = 0;
+
+    while (multiplier > 0) {
+       if (multiplier & 1 == 1) {
+            product += multiplicand;
+        }
+
+        multiplier >>= 1;
+        multiplicand <<= 1;
+    }
+
+    printf("Product is %d\n", product);
+}
