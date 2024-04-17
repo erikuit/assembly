430d8a10285b04be09f901390f8239e498b0e28e
Erik Uittenbogaard <erik@becurious.com>
2024-04-17 18:54:13 +0200

    Clang format

--- divide.c
+++ divide.c
@@ -2,29 +2,29 @@
 #include <stdlib.h>
 
 int main(int argc, char *argv[]) {
-   if (argc < 3) {
-        printf("Usage: %s <remainder> <divisor>\n", argv[0]);
-        return 1;
+  if (argc < 3) {
+    printf("Usage: %s <remainder> <divisor>\n", argv[0]);
+    return 1;
+  }
+
+  int remainder = atoi(argv[1]);
+  int divisor = atoi(argv[2]) << 16;
+  int quotient = 0;
+
+  for (int i = 0; i < 17; i++) {
+    remainder -= divisor;
+
+    if (remainder < 0) {
+      remainder += divisor;
+      quotient <<= 1;
+      quotient |= 0;
+    } else {
+      quotient <<= 1;
+      quotient |= 1;
     }
 
-    int remainder = atoi(argv[1]);
-    int divisor = atoi(argv[2]) << 16;
-    int quotient = 0;
+    divisor >>= 1;
+  }
 
-   for (int i = 0; i < 17; i++) {
-      remainder -= divisor;
-
-      if (remainder < 0) {
-         remainder += divisor;
-         quotient <<= 1;
-         quotient |= 0;
-      } else {
-         quotient <<= 1;
-         quotient |= 1;
-      }
-
-      divisor >>= 1;
-   }
-
-    printf("Quotient is %d, remainder is %d\n", quotient, remainder);
+  printf("Quotient is %d, remainder is %d\n", quotient, remainder);
 }
--- multiply.c
+++ multiply.c
@@ -2,23 +2,23 @@
 #include <stdlib.h>
 
 int main(int argc, char *argv[]) {
-   if (argc < 2) {
-        printf("Usage: %s <multiplier> <multiplicand>\n", argv[0]);
-        return 1;
+  if (argc < 2) {
+    printf("Usage: %s <multiplier> <multiplicand>\n", argv[0]);
+    return 1;
+  }
+
+  int multiplier = atoi(argv[1]);
+  int multiplicand = atoi(argv[2]);
+  int product = 0;
+
+  for (int i = 0; i < 32; i++) {
+    if ((multiplier & 1) == 1) {
+      product += multiplicand;
     }
 
-    int multiplier = atoi(argv[1]);
-    int multiplicand = atoi(argv[2]);
-    int product = 0;
+    multiplier >>= 1;
+    multiplicand <<= 1;
+  }
 
-    while (multiplier > 0) {
-       if (multiplier & 1 == 1) {
-            product += multiplicand;
-        }
-
-        multiplier >>= 1;
-        multiplicand <<= 1;
-    }
-
-    printf("Product is %d\n", product);
+  printf("Product is %d\n", product);
 }
