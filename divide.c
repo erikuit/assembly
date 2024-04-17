1935fb81e799d208d4da06b57a76e3122bd36c44
Erik Uittenbogaard <erik@becurious.com>
2024-04-17 18:36:36 +0200

    Division in C

---
+++ divide.c
@@ -0,0 +1,30 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main(int argc, char *argv[]) {
+   if (argc < 3) {
+        printf("Usage: %s <remainder> <divisor>\n", argv[0]);
+        return 1;
+    }
+
+    int remainder = atoi(argv[1]);
+    int divisor = atoi(argv[2]) << 16;
+    int quotient = 0;
+
+   for (int i = 0; i < 17; i++) {
+      remainder -= divisor;
+
+      if (remainder < 0) {
+         remainder += divisor;
+         quotient <<= 1;
+         quotient |= 0;
+      } else {
+         quotient <<= 1;
+         quotient |= 1;
+      }
+
+      divisor >>= 1;
+   }
+
+    printf("Quotient is %d, remainder is %d\n", quotient, remainder);
+}
