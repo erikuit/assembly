a248d4b449e45b1180c9d91d798101a9e4832936
erikuit <erik@becurious.com>
2024-05-11 11:23:53 +0200

    Use printf in argc

--- argc.s
+++ argc.s
@@ -1,6 +1,16 @@
-.global _start
+.global main
 
-_start:
-    lw a0, 0(sp)
-    addi a7, zero, 93
+.section .rodata
+fmt:    .string "Number of arguments is %d\n"
+.section .text
+
+main:
+    add t0, x0, a0
+    add t1, x0, a1
+    addi t2, x0, 1
+    add a1, x0, t0
+    la a0, fmt
+    call printf
+    addi a0, x0, 0
+    addi a7, x0, 93
     ecall
