c5b041cff8176dda88c08abf20ab2cf9daaf61ea
erikuit <erik@becurious.com>
2024-05-14 10:40:56 +0200

    Simplify branching

--- multiply.s
+++ multiply.s
@@ -5,35 +5,26 @@ fmt:    .string "Product is %d\n"
 .section .text
 
 main:
-    addi t0, x0, 19 
-    addi t1, x0, 9
-    addi t2, x0, 0 
+    addi t0, x0, 19 # Multiplicand
+    addi t1, x0, 9  # Multiplier
+    addi t2, x0, 0  # Product
     addi t4, x0, 1
 
-    bne t0, x0, Loop
-
-    jal x0, End
-
 Loop:
-    andi t3, t0, 1
-    beq t3, t4, Add
-    jal x0, Shift
+    andi t3, t0, 1    # Multiplier & 1
+    bne t3, t4, Shift # If multiplier & 1 != 1
+    add t2, t2, t1
 
 Shift:
     srli t0, t0, 1
     slli t1, t1, 1
-
-    beq t0, x0, End
-    beq x0, x0, Loop
-
-Add:
-    add t2, t2, t1
-    jal x0, Shift
+    bne t0, x0, Loop
 
 End:
     la a0, fmt
     add a1, x0, t2
     call printf
+
     addi a0, x0, 0
     addi a7, x0, 93
     ecall
