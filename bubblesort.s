a1c2bd28f62ede6131530649b983b8f8785ba399
erikuit <erik@becurious.com>
2024-05-19 10:48:45 +0200

    Loops

--- bubblesort.s
+++ bubblesort.s
@@ -1,21 +1,42 @@
 .global main
 
 .section .rodata
-fmt:    .string "Result is %d\n"
+fmt:    .string "I is %d, J is %d\n"
 .section .text
 arr:   .word 64, 34, 25, 12, 22, 11, 90
 arrsize: .word 7
 
 main:
     la x5, arr
-    lw x6, arrsize
+    lw x22, arrsize
     lw x7, 0(x5)
     addi x20, x0, 0 # i
+
+L1:
     addi x21, x0, 0 # j
+
+L2:
+    addi x20, x20, 1
+    bne x20, x22, L3
+    bne x20, x22, L2
+
+    j End
+
+L3:
+    addi x21, x21, 1
+
+    la x10, fmt
+    add x11, x0, x20
+    add x12, x0, x21
+    call printf
+
+    bne x21, x22, L3
+    j L1
 
 End:
     la x10, fmt
-    add x11, x0, x7
+    addi x11, x0, 88
+    addi x12, x0, 99
     call printf
 
     addi a0, x0, 0
